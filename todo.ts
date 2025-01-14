5a94032681be7691c11f0f375e4d1779228c1d39
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-01-14 00:16:02 -0300

    feat: commands
    ls, rm, add

---
+++ todo.ts
@@ -0,0 +1,159 @@
+#!/usr/bin/env bun
+import fs from "node:fs";
+import { resolve } from "node:path";
+import inquirer from "inquirer";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+// Defina o caminho para o arquivo todo.md
+const TODO_FILE = resolve(`${process.cwd()}/todo.md`);
+
+// Função para criar o arquivo todo.md, se não existir
+const createTodoFile = () => {
+	if (!fs.existsSync(TODO_FILE)) {
+		fs.writeFileSync(TODO_FILE, "# Todo`s\n\n");
+		console.log("Arquivo todo.md criado.");
+	}
+};
+
+// Função para adicionar uma tarefa ao arquivo todo.md
+const addTask = (task: string) => {
+	if (!task) {
+		console.log("Por favor, forneça uma tarefa para adicionar.");
+		return;
+	}
+
+	// Adiciona a tarefa no arquivo todo.md
+	fs.appendFileSync(TODO_FILE, `- [ ] ${task}\n`);
+	console.log(`Tarefa '${task}' adicionada com sucesso.`);
+};
+
+const list = async () => {
+	if (!fs.existsSync(TODO_FILE)) {
+		console.log("Arquivo todo.md não encontrado.");
+		return;
+	}
+
+	// Lê o conteúdo do arquivo e encontra as tarefas pendentes
+	const tasks = fs
+		.readFileSync(TODO_FILE, "utf-8")
+		.split("\n")
+		.filter((line) => line.includes("- [ ]") || line.includes("- [x]"));
+
+	if (tasks.length === 0) {
+		console.log("Nenhuma tarefa encontrada.");
+		return;
+	}
+
+	const choices = tasks.map((task) => ({
+		name: task.replace("- [ ] ", "").replace("- [x] ", ""),
+		value: task,
+		checked: task.includes("- [x]"),
+	}));
+
+	const answers: { tasksDone: string[] } = await inquirer.prompt([
+		{
+			type: "checkbox",
+			name: "tasksDone",
+			message: "Selecione as tarefas concluídas:",
+			choices,
+		},
+	]);
+
+	const updatedTasks = answers.tasksDone;
+
+	if (updatedTasks.length <= 0) {
+		return console.log("Nenhum tarefa concluída");
+	}
+
+	choices.map((task) => {
+		const taskDone = updatedTasks.includes(task.value);
+
+		const updatedTask = taskDone
+			? task.value.replace("- [ ]", "- [x]")
+			: task.value.replace("- [x]", "- [ ]");
+
+		const fileContent = fs.readFileSync(TODO_FILE, "utf-8");
+		const newContent = fileContent.replace(task.value, updatedTask);
+		fs.writeFileSync(TODO_FILE, newContent);
+	});
+
+	console.log(`${updatedTasks.length} tarefas concluídas`);
+};
+
+const removeTasks = async () => {
+	if (!fs.existsSync(TODO_FILE)) {
+		console.log("Arquivo todo.md não encontrado.");
+		return;
+	}
+
+	// Lê o conteúdo do arquivo e encontra as tarefas pendentes
+	const tasks = fs
+		.readFileSync(TODO_FILE, "utf-8")
+		.split("\n")
+		.filter((line) => line.includes("- [ ]") || line.includes("- [x]"));
+
+	if (tasks.length === 0) {
+		console.log("Nenhuma tarefa encontrada.");
+		return;
+	}
+
+	const choices = tasks.map((task) => ({
+		name: task.replace("- [ ] ", "").replace("- [x] ", ""),
+		value: task,
+	}));
+
+	const answers: { taskToRemove: string[] } = await inquirer.prompt([
+		{
+			type: "checkbox",
+			name: "taskToRemove",
+			message: "Selecione as tarefas a serem removidas:",
+			choices,
+		},
+	]);
+
+	const updatedTasks = answers.taskToRemove;
+
+	if (updatedTasks.length <= 0) {
+		return console.log("Nenhum tarefa removida");
+	}
+
+	choices.map((task) => {
+		const taskRemove = updatedTasks.includes(task.value);
+		if (taskRemove) {
+			const fileContent = fs.readFileSync(TODO_FILE, "utf-8");
+			const newContent = fileContent.replace(`\n${task.value}`, "");
+			fs.writeFileSync(TODO_FILE, newContent);
+		}
+	});
+
+	console.log(`${updatedTasks.length} tarefas removidas`);
+};
+
+// Definir os comandos do yargs
+yargs(hideBin(process.argv))
+	.command({
+		command: "add",
+		describe: "Adicionar uma tarefa",
+		handler(argv) {
+			createTodoFile();
+			addTask(argv._.slice(1).join(" ") as string);
+		},
+	})
+	.command({
+		command: "ls",
+		describe: "Listar todas as tarefas",
+		handler() {
+			createTodoFile();
+			list();
+		},
+	})
+	.command({
+		command: "rm",
+		describe: "Remover tarefas",
+		handler() {
+			createTodoFile();
+			removeTasks();
+		},
+	})
+	.parse();
